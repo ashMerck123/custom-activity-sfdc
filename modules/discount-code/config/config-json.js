bae3a77c3d1919ab3fe14696b8fe78b124508b21
ashMerck123 <69150885+ashMerck123@users.noreply.github.com>
2020-11-03 12:50:30 +0100

    Update config-json.js

--- modules/discount-code/config/config-json.js
+++ modules/discount-code/config/config-json.js
@@ -18,9 +18,7 @@ module.exports = function configJSON(req) {
       execute: {
         // See: https://developer.salesforce.com/docs/atlas.en-us.mc-apis.meta/mc-apis/how-data-binding-works.htm
         inArguments: [
-                              {
-                    "contactIdentifier": "{{Contact.Indicator}}"
-                },
+                              {"contactIdentifier": "{{Contact.Indicator}}"},
         ],
         outArguments: [],
         // Fill in the host with the host that this is running on.
